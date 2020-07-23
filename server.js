0c1c974ca39e40d5e34bd10ad6af6f1b07c85136
cybercode2000 <damicodd123@gmail.com>
2020-07-23 13:10:35 -0400

    first task done

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
+  if (req.method === 'GET' && req.url == "/") {
       res.write("Hello World, welcome to WeJapa Internships");
       res.end()
   }
+  else {
+      res.end('Error 404!!!!!! PAGE NOT FOUND')
+    }
 });
 
 PORT = 3000;
 
-server.listen(PORT);
-
-console.log(`App Now Listen @ PORT ${PORT}`)
+server.listen(PORT, () => {
+    console.log(`App Now Listen @ PORT ${PORT}`)
+});
